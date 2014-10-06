e33fc37f4c3e373906dc39437551eaef49bf36e8
yoshikyoto <admin@h96.26.238.10.30790.vlan.kuins.net>
2014-10-06 14:19:58 +0900

    initial commit

---
+++ init.el
@@ -0,0 +1,11 @@
+; Setting for Twitter
+
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
+(require 'twittering-mode)
+
+(setq twittering-account-authorization 'authorized)
+(setq twittering-oauth-access-token-alist
+'(("oauth_token" . "83811178-wYRMTw9hAr6rAMWG0cOAYYfR6LcPoeiZxLuvaqxVM")
+ ("oauth_token_secret" . "HvZ9u7rYs19dvWxIfOuVhduPrlb59z7tMb9NOAEaUboDD")
+ ("user_id" . "83811178")
+ ("screen_name" . "yoshiki_utakata")))
