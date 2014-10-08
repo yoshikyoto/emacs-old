c074bf12e3251b8c54cf8e32c43957a5bb1c933e
yoshikyoto <admin@h96.26.238.10.30790.vlan.kuins.net>
2014-10-09 07:09:05 +0900

    little change

--- init.el
+++ init.el
@@ -66,11 +66,6 @@
 (ac-config-default)
 
 ; Setting for Emacs twittering mode
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/twittering-mode"))
 (require 'twittering-mode)
-(setq twittering-account-authorization 'authorized)
-(setq twittering-oauth-access-token-alist
-'(("oauth_token" . "83811178-wYRMTw9hAr6rAMWG0cOAYYfR6LcPoeiZxLuvaqxVM")
- ("oauth_token_secret" . "HvZ9u7rYs19dvWxIfOuVhduPrlb59z7tMb9NOAEaUboDD")
- ("user_id" . "83811178")
- ("screen_name" . "yoshiki_utakata")))
+(load "~/.emacs.d/twittering-mode-config.el")
